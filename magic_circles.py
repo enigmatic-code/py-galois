255c986cedd086d35cef95b7ebc82672765dd206
Jim Randell <jim.randell@gmail.com>
2019-11-02 09:23:47 +0000

    added verify_magic_circle().

--- magic_circles.py
+++ magic_circles.py
@@ -28,7 +28,8 @@ def generator(f):
 def perfect_difference_set(n):
 
   # make the galois field GF(N) where N = n^3
-  f = GF(n ** 3, cached=0)
+  n3 = n ** 3
+  f = GF(n3, cached=0)
   #printf("[perfect_difference_set: using field {f} ...]")
 
   # find an element of GF(N) that generates GF*(N)
@@ -42,7 +43,7 @@ def perfect_difference_set(n):
 
   # make the pds
   pds = [0, 1]
-  for (i, x) in enumerate(powers(f, g, n ** 3, 2), start=2):
+  for (i, x) in enumerate(powers(f, g, n3, 2), start=2):
     if f.sub(x, g) in fstar:
       pds.append(i % m)
 
@@ -51,7 +52,7 @@ def perfect_difference_set(n):
         
 
 # generate magic circles of size n
-def magic(n):
+def magic_circle(n):
 
   # make a perfect difference set of the required size
   pds = perfect_difference_set(n - 1)
@@ -71,6 +72,19 @@ def magic(n):
       yield s
       seen[s] = 1
 
+def verify_magic_circle(s):
+  n = len(s)
+  ss = set()
+  # consider k consecutive sectors
+  for k in irange(1, n - 1):
+    # start sector
+    for i in irange(0, n - 1):
+      ss.add(sum(s[(i + j) % n] for j in irange(0, k - 1)))
+  # the whole circle
+  ss.add(sum(s))
+  # are all numbers represented?
+  return len(ss) == n * (n - 1) + 1
+
 
 if __name__ == "__main__" or __name__ == "<run_path>":
 
@@ -79,6 +93,7 @@ if __name__ == "__main__" or __name__ == "<run_path>":
   n = arg(8, 0, int)
   printf("[n={n}]")
 
-  for (i, s) in enumerate(magic(n), start=1):
+  for (i, s) in enumerate(magic_circle(n), start=1):
     printf("{i}: {s}")
+    assert verify_magic_circle(s)
   printf("[n={n}: found {i} magic circles]")
